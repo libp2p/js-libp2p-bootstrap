43c79c8e61c655ae8516efd4b04d3dc45879bfca
Omidiora Samuel <>
2019-08-25 00:32:08 +0100

    fix: remove unused packages

--- test/node.js
+++ test/node.js
@@ -1,16 +1,15 @@
-// The typings test should run only in the node
-// environment
-const ts = require("typescript");
-const tt = require("typescript-definition-tester")
-const fs = require("fs")
-const chai = require("chai")
+/* eslint-env mocha */
+'use strict'
 
+const tt = require('typescript-definition-tester')
+// typings test should run only in the node
+// environment due to reading of files
 describe('typings declaration tests', () => {
-    it('should compile typings examples successfully against types.d.ts', (done) => {
-        tt.compileDirectory(
-            __dirname + '/typings',
-            (fileName) => fileName.indexOf('.ts') > -1,
-            (error) => done(error)
-            );
-    });
-});
+  it('should compile typings examples successfully against types.d.ts', (done) => {
+    tt.compileDirectory(
+      `${__dirname}/typings`,
+      (fileName) => fileName.indexOf('.ts') > -1,
+      (error) => done(error)
+    )
+  })
+})
